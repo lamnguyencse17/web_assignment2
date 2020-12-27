80e47092244081fbb4317c67d12b4456cd45c37f
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-12-27 15:03:05 +0700

    Register is ready

--- register.php
+++ register.php
@@ -1,4 +1,11 @@
 <?php
+$mysqli = new mysqli("localhost", "root", "", "TCH");
+
+// Check connection
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit();
+}
 function hashPassword($password)
 {
     return password_hash($password, PASSWORD_BCRYPT);
@@ -8,12 +15,18 @@ function validateRegister($email, $password)
 {
     $errMsg = new stdClass();
     $errMsg->email = "";
-    $errMsg->email = "";
+    $errMsg->password = "";
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $errMsg->email = "Invalid Email";
     }
+    if (strlen($email) > 30){
+        $errMsg->email = "Email is too long";
+    }
     if (empty($password) || strlen($password) < 6) {
         $errMsg->email = "Invalid Password";
+    }
+    if (strlen($password) > 30){
+        $errMsg->password = "Password is too long";
     }
     return $errMsg;
 }
@@ -22,9 +35,24 @@ $email = $_POST["email"];
 $password = $_POST["password"];
 $errMsg = validateRegister($email, $password);
 if ($errMsg->email !== "" || $errMsg->password !== ""){
-    $errMsg->message->email = $errMsg->email;
-    $errMsg->message->password = $errMsg->password;
     $returnData = json_encode((array)$errMsg);
     echo $returnData;
     return;
 }
+$password=hashPassword($password);
+$query = $mysqli->prepare("insert into tch.account(email, password) values (?, ?)") ;
+$query->bind_param("ss", $email, $password);
+$message = $query->execute();
+$returnData = new stdClass();
+if ($message === 1){
+    http_response_code(201);
+    $returnData->message = "Successfully registered";
+    $returnData = json_encode((array)$returnData);
+    echo $returnData;
+    return;
+}
+http_response_code(400);
+$returnData->message = "Email is duplicated";
+$returnData = json_encode((array)$returnData);
+echo $returnData;
+return;
