b36e54a7e87abba87f7bfe6662f8d191da205ef3
Dao Trong Tien <tien.dao1021999@hcmut.edu.vn>
2020-12-28 12:15:41 +0700

    Login feature

--- login.php
+++ login.php
@@ -1,1 +1,60 @@
 <?php
+$mysqli = new mysqli("localhost", "root", "", "TCH");
+
+// Check connection
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit();
+}
+function hashPassword($password)
+{
+    return password_hash($password, PASSWORD_BCRYPT);
+}
+
+function validateRegister($email, $password)
+{
+    $errMsg = new stdClass();
+    $errMsg->email = "";
+    $errMsg->password = "";
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errMsg->email = "Invalid Email";
+    }
+    if (strlen($email) > 30){
+        $errMsg->email = "Email is too long";
+    }
+    if (empty($password) || strlen($password) < 6) {
+        $errMsg->password = "Invalid Password";
+    }
+    if (strlen($password) > 30){
+        $errMsg->password = "Password is too long";
+    }
+    return $errMsg;
+}
+
+$email = $_POST["email"];
+$password = $_POST["password"];
+$errMsg = validateRegister($email, $password);
+if ($errMsg->email !== "" || $errMsg->password !== ""){
+    $returnData = json_encode((array)$errMsg);
+    echo $returnData;
+    return;
+}
+$query = $mysqli->prepare("SELECT * FROM account WHERE email=?" );
+$query->bind_param("s",$email);
+$message = $query->execute();
+$res = $query->get_result();
+$user = $res->fetch_object();
+$returnData = new stdClass();
+if(password_verify($password,$user->password)){
+    http_response_code(200);
+    $returnData->message = "Login successfully!";
+    $returnData = json_encode((array)$returnData);
+    echo $returnData;
+    return;
+} else{
+    http_response_code(400);
+    $returnData->message = "Wrong email or password!";
+    $returnData = json_encode((array)$returnData);
+    echo $returnData;
+    return;
+}
